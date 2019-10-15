bd7f465119b155797a0376b2aa05e5bc1b238096
Courtney <lrccoordntr@aol.com>
2019-10-15 16:28:51 -0500

    Updated Folder.js

---
+++ src/containers/Folder.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import FolderContainer from '../components/FolderContainer'
+import UploadButton from '../components/UploadButton'
+
+const Folder = () => {
+  return (
+    <React.Fragment>
+      <FolderContainer />
+      <UploadButton />
+    </React.Fragment>
+  )
+}
+
+export default Folder
