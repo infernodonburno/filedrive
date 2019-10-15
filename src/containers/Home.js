c3727b80ba617c295d0e62fa606c2064923f863f
Chris Purnell <sultansofrock@gmail.com>
2019-10-15 15:43:12 -0500

    Added a folder container

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
 import DownloadButton from '../components/DownloadButton'
+import FolderContainer from '../components/FolderContainer'
 
 const Home = () => {
-  return <DownloadButton />
+  return (
+  <React.Fragment>
+    <DownloadButton />
+  </React.Fragment>
+  )
 }
 
 export default Home
