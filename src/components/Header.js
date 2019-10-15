a43b84865dcd4b5ef6ac94b6eac230014e21b5b9
Chris Purnell <sultansofrock@gmail.com>
2019-10-15 17:27:59 -0500

    Centered header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+  
+`
 const Header = props => (
-  <header>
+  <Container>
     <h1>File Drive</h1>
     <br />
-  </header>
+  </Container>
 )
 
 export default Header
