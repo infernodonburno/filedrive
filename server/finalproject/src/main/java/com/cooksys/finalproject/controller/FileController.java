c9f787155e718a564376ca1cd32ee636c2fdbd0b
hwangjk <hwangjk012@hotmail.com>
2019-10-15 16:08:18 -0500

    Working PATCH trash

--- server/finalproject/src/main/java/com/cooksys/finalproject/controller/FileController.java
+++ server/finalproject/src/main/java/com/cooksys/finalproject/controller/FileController.java
@@ -2,6 +2,7 @@ package com.cooksys.finalproject.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
