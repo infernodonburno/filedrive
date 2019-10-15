ce12c896c435282de0a6f901d8f7549806ad0a0f
Chris Purnell <sultansofrock@gmail.com>
2019-10-15 16:35:37 -0500

    Added a navbar

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { Route } from 'react-router-dom'
 
 import Header from './components/Header'
 import Home from './containers/Home'
-
+import NavBar from './components/Navbar'
 const App = props => (
   <div>
+    <NavBar />
     <Route path='/' component={Header} />
     <Route path='/' component={Home} />
   </div>
