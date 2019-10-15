6a47538a345146bc795dc8882215878054ed7b91
Chris Purnell <sultansofrock@gmail.com>
2019-10-15 13:38:40 -0500

    Added Move duck

--- src/ducks/move.duck.js
+++ src/ducks/move.duck.js
@@ -1,1 +1,43 @@
-export const MOVE_FILE = 'MOVE_FILE'
+export const MOVE_FILE_DONE = 'MOVE_FILE_DONE'
+export const MOVE_FILE_FAILURE = 'MOVE_FILE_FAILURE'
+export const MOVE_FILE_BEGIN = 'MOVE_FILE_BEGIN'
+
+const initialState = {
+  files: [],
+  errorMovingFile: false
+}
+
+export default function reducer (state = initialState, action) {
+  switch (action.type) {
+    case MOVE_FILE_DONE:
+      return {
+        ...state,
+        errorMovingFiles: false,
+        files: action.payload.files
+      }
+    case MOVE_FILE_FAILURE:
+      return {
+        ...state,
+        files: initialState.files
+      }
+    default:
+      return state
+  }
+}
+
+export const viewFileDone = (files) => ({
+  type: MOVE_FILE_DONE,
+  payload: {
+    files
+  }
+})
+
+export const viewFolderDone = () => ({
+  type: MOVE_FILE_FAILURE
+})
+
+const moveFileBegin = () => ({
+  type: MOVE_FILE_BEGIN
+})
+
+// TOTO API FETCH FOR FILES TO MOVE
