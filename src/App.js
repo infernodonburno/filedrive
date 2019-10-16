b634a5e4d535f680a4b0ffcb5179c79117e9ac79
Courtney <lrccoordntr@aol.com>
2019-10-15 20:31:37 -0500

    Added route for Trash

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import { Route } from 'react-router-dom'
 
 import Header from './components/Header'
 import Home from './containers/Home'
+import Trash from './containers/Trash'
 
 const App = props => (
   <div>
     <Route path='/' component={Header} />
-    <Route path='/' component={Home} />
+    <Route exact path='/' component={Home} />
+    <Route path='/trash/' component={Trash} />
   </div>
 )
 
