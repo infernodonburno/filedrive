3799ecb6fce1a006b4e613989dd95e4bc34b38b0
Courtney <lrccoordntr@aol.com>
2019-10-16 10:45:52 -0500

    Added FileContainer to Home; imported React in Trash

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+
+import FileContainer from '../components/FileContainer'
 import FolderContainer from '../components/FolderContainer'
 import UploadButton from '../components/UploadButton'
 
@@ -7,6 +9,7 @@ const Home = () => {
   return (
     <React.Fragment>
       <FolderContainer />
+      <FileContainer />
       <UploadButton />
     </React.Fragment>
   )
