66e9cbadb25888195d7ff0dda5849b2bb7dedce2
hwangjk <hwangjk012@hotmail.com>
2019-10-20 19:12:19 -0500

    upload nested folders

--- server/finalproject/src/main/java/com/cooksys/finalproject/service/FolderService.java
+++ server/finalproject/src/main/java/com/cooksys/finalproject/service/FolderService.java
@@ -62,7 +62,8 @@ public class FolderService {
     	// create the folder requested if mapped to root
 		FolderEntity folderToCreate = folderMapper.dtoToEntity(folderRequest);
 		FolderEntity folder = folderRepository.saveAndFlush(folderToCreate);
-
+		
+		System.out.print("GOT HERE");
 		for (FileRequestDto fileRequest : folderRequest.getFiles()) {
 			FileEntity fileToCreate = fileMapper.dtoToEntity(fileRequest);
 			fileToCreate.setFolder(folder);
