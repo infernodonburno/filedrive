df55ca0eb2ccabcf14f07417328c5b9ead426485
Chris <sultansofrock@gmail.com>
2019-10-22 09:07:21 -0500

    Filedrive in header navbar now navigates to the home page

--- client/landing-page/src/components/GlobalHeader.js
+++ client/landing-page/src/components/GlobalHeader.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
+import { Link } from 'react-router-dom'
 
 const HeaderAppBar = props => {
     return (
     <AppBar>
-        FileDrive
+       <Link to='/' style={{ textDecoration: 'none', color: 'white',  }}>
+       FileDrive 
+       </Link>
     </AppBar>
     )
 }
