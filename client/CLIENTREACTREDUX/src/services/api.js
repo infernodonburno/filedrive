9758d1dbf5b8b968fcb705801271ca7678291e58
Courtney <lrccoordntr@aol.com>
2019-10-23 18:34:13 -0500

    Updating fetches

--- client/CLIENTREACTREDUX/src/services/api.js
+++ client/CLIENTREACTREDUX/src/services/api.js
@@ -1,10 +1,14 @@
 import request from '../utils/request'
 
 const SERVER_ROOT = 'http://localhost:8080'
+const header = {
+  Authorization:
+    'Bearer eyJraWQiOiJpZXA2dmZuSTJGWVN5VDFBS3Z1ZENsRjhfWDh0MFRFT2VvREQtcTFyYXNJIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULmhocGxfVDJOZmxtSk9YOGRCN29UYXpPajB4TDlORllHRGJ6Q2xWT1ZSNVkiLCJpc3MiOiJodHRwczovL2Rldi0yMDUwNzMub2t0YS5jb20vb2F1dGgyL2RlZmF1bHQiLCJhdWQiOiJhcGk6Ly9kZWZhdWx0IiwiaWF0IjoxNTcxODcwNzA2LCJleHAiOjE1NzE4NzQzMDYsImNpZCI6IjBvYTFtdTUxNzREWXVMSGpNMzU3IiwidWlkIjoiMDB1MW1yeTFlMzdFQ0lTeGEzNTciLCJzY3AiOlsib3BlbmlkIiwicHJvZmlsZSIsImVtYWlsIl0sInN1YiI6ImZpbGVkcml2ZXByb2plY3Rjb29rc3lzQGdtYWlsLmNvbSJ9.mSJoLjLO31RyDATzMgjpVAqJgTtb29kC5d_3Lw_iD_oniGWI-T-wJpebzKY0NZHjmsL8L7bpfhSoEJsY0DRYULPC_E1maGTCoP8TEhOAHY-D-07OD2bubrsY5nxTn4yUzmmncnMP9p5LraPF_UNXU20KVHZWH8rt4O9VaVZOnIg2MsCagl5uqFykccMg1FyWo81klEyPgT9FBYuP6WdbfDJnKApqlN4BXVWc4kFaCXj4twP5uWrvpoYZPqHaaNvlkHX7PerBy-2KzdYjJWFQnrc1Eyd9BQG1c3wMBCBVEILyK7FhpWvCUPdYKVM66AsIfezrgvn7OYdpFEcOJBYGTQ'
+}
 
 // Fetch all files from root folder
 export function fetchFiles (folderID) {
-  return fetchFromServer(`folders/${folderID}`)
+  return fetchFromServer(`files/${folderID}/all`)
 }
 
 // Fetch all folders from root
@@ -32,10 +36,18 @@ export function fetchTrashFolders () {
   return fetchFromServer('folders/trash')
 }
 
-function fetchFromServer (endpoint, options) {
+function fetchFromServer (endpoint) {
   let url = [SERVER_ROOT, endpoint].join('/')
   console.log(url)
-  return request(url)
+  const options = {
+    method: 'GET',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      Authorization:
+        'Bearer eyJraWQiOiJpZXA2dmZuSTJGWVN5VDFBS3Z1ZENsRjhfWDh0MFRFT2VvREQtcTFyYXNJIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULmhocGxfVDJOZmxtSk9YOGRCN29UYXpPajB4TDlORllHRGJ6Q2xWT1ZSNVkiLCJpc3MiOiJodHRwczovL2Rldi0yMDUwNzMub2t0YS5jb20vb2F1dGgyL2RlZmF1bHQiLCJhdWQiOiJhcGk6Ly9kZWZhdWx0IiwiaWF0IjoxNTcxODcwNzA2LCJleHAiOjE1NzE4NzQzMDYsImNpZCI6IjBvYTFtdTUxNzREWXVMSGpNMzU3IiwidWlkIjoiMDB1MW1yeTFlMzdFQ0lTeGEzNTciLCJzY3AiOlsib3BlbmlkIiwicHJvZmlsZSIsImVtYWlsIl0sInN1YiI6ImZpbGVkcml2ZXByb2plY3Rjb29rc3lzQGdtYWlsLmNvbSJ9.mSJoLjLO31RyDATzMgjpVAqJgTtb29kC5d_3Lw_iD_oniGWI-T-wJpebzKY0NZHjmsL8L7bpfhSoEJsY0DRYULPC_E1maGTCoP8TEhOAHY-D-07OD2bubrsY5nxTn4yUzmmncnMP9p5LraPF_UNXU20KVHZWH8rt4O9VaVZOnIg2MsCagl5uqFykccMg1FyWo81klEyPgT9FBYuP6WdbfDJnKApqlN4BXVWc4kFaCXj4twP5uWrvpoYZPqHaaNvlkHX7PerBy-2KzdYjJWFQnrc1Eyd9BQG1c3wMBCBVEILyK7FhpWvCUPdYKVM66AsIfezrgvn7OYdpFEcOJBYGTQ'
+    }
+  }
+  return request(url, options)
 }
 
 export function postFile (file, folderID) {
