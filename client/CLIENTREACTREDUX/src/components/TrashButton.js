8bc18acc7873ec1fb2a27d918a713fff5b317a1c
Courtney <lrccoordntr@aol.com>
2019-10-24 11:18:18 -0500

    Fix trashbutton icon in folders

--- client/CLIENTREACTREDUX/src/components/TrashButton.js
+++ client/CLIENTREACTREDUX/src/components/TrashButton.js
@@ -5,7 +5,7 @@ const TrashButton = props => {
     <div>
       <input
         type='image'
-        src='./resources/trashicon.png'
+        src='http://localhost:3000/resources/trashicon.png'
         onClick={props.onClick}
         alt='trashbutton'
       />
