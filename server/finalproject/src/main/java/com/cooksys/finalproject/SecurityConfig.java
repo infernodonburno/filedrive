44f1606c72a977fe5a2d79b7732d5858e7364a4f
hwangjk <hwangjk012@hotmail.com>
2019-10-23 19:29:20 -0500

    nothing

--- server/finalproject/src/main/java/com/cooksys/finalproject/SecurityConfig.java
+++ server/finalproject/src/main/java/com/cooksys/finalproject/SecurityConfig.java
@@ -1,18 +1,19 @@
 package com.cooksys.finalproject;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.web.cors.CorsConfiguration;
 
 @EnableWebSecurity
-@EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-	    http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
-	}
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+          .antMatchers(HttpMethod.OPTIONS);
+      }
 }
