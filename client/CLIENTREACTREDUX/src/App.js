7e6132f066af4d4ff0bcb5d29ad843e37af1bfb6
hwangjk <hwangjk012@hotmail.com>
2019-10-24 02:09:50 -0500

    cleaned up files and integrated landing code to react code

--- client/CLIENTREACTREDUX/src/App.js
+++ client/CLIENTREACTREDUX/src/App.js
@@ -8,8 +8,24 @@ import Trash from './containers/Trash'
 import Folder from './containers/Folder'
 import SidebarExample from './components/Breadcrumb'
 
+import './App.css'
+import { Security, ImplicitCallback } from '@okta/okta-react'
+
+import Login from './containers/Login'
+
+const config = {
+  issuer: 'https://dev-205073.okta.com/oauth2/default',
+  redirectUri: window.location.origin + '/implicit/callback',
+  clientId: '0oa1mu5174DYuLHjM357',
+  pkce: true
+}
 const App = props => (
   <div>
+    <Security {...config}>
+      <Route path='/' exact component={Login} />
+      <Route path='/login' exact component={Login} />
+      <Route path='/implicit/callback' component={ImplicitCallback} />
+    </Security>
     <NavBar />
     <SidebarExample />
     <Route path='/' component={Header} />
