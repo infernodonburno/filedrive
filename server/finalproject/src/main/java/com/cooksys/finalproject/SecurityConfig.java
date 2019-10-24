9cb1b1915c8ed9b5ef0148c4ab94348763be9424
hwangjk <hwangjk012@hotmail.com>
2019-10-23 19:03:11 -0500

    fixed cors i think

--- server/finalproject/src/main/java/com/cooksys/finalproject/SecurityConfig.java
+++ server/finalproject/src/main/java/com/cooksys/finalproject/SecurityConfig.java
@@ -1,10 +1,18 @@
 package com.cooksys.finalproject;
 
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.cors.CorsConfiguration;
 
 @EnableWebSecurity
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig {}
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+	    http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+	}
+}
