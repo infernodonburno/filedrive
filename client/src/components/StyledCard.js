b48601d7737e81a08141455da78c9dd058e28216
Will Marttala <wmarttala123@gmail.com>
2019-10-24 10:36:30 -0500

    Updated the title of the page and reduced the size of the StyleCard component

--- client/src/components/StyledCard.js
+++ client/src/components/StyledCard.js
@@ -3,13 +3,13 @@ import styled from 'styled-components'
 const StyledCard = styled.div`
   box-shadow: 5px 5px 5px #ccc;
   border: 1px solid #ccc;
-  height: 2000px;
+  height: 400px;
   width: 750px;
   overflow: scroll;
+  padding: 50px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
   margin: 0 auto;
 `
 
