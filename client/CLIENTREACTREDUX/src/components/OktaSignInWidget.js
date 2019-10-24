c3e553eab4aeda7d199e3bbc5cb5f797ba77f152
Chris <sultansofrock@gmail.com>
2019-10-24 10:50:14 -0500

    Changed login page with a redirect button to file drive

--- client/CLIENTREACTREDUX/src/components/OktaSignInWidget.js
+++ client/CLIENTREACTREDUX/src/components/OktaSignInWidget.js
@@ -1,47 +1,49 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import OktaSignIn from '@okta/okta-signin-widget';
-import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import OktaSignIn from '@okta/okta-signin-widget'
+import '@okta/okta-signin-widget/dist/css/okta-sign-in.min.css'
 
 export default class OktaSignInWidget extends Component {
-  componentDidMount() {
-    const el = ReactDOM.findDOMNode(this);
+  componentDidMount () {
+    const el = ReactDOM.findDOMNode(this)
     this.widget = new OktaSignIn({
       baseUrl: 'https://dev-205073.okta.com',
       logo: '/logo.png',
+      redirectUri: 'http://localhost:3000/home',
       authParams: {
         pkce: true
-    },
-    registration: {
-        parseSchema: function(schema, onSuccess, onFailure) {
-           // handle parseSchema callback
-           onSuccess(schema);
+      },
+      registration: {
+        parseSchema: function (schema, onSuccess, onFailure) {
+          // handle parseSchema callback
+          onSuccess(schema)
         },
         preSubmit: function (postData, onSuccess, onFailure) {
-           // handle preSubmit callback
-           onSuccess(postData);
+          // handle preSubmit callback
+          onSuccess(postData)
         },
         postSubmit: function (response, onSuccess, onFailure) {
-            // handle postsubmit callback
-           onSuccess(response);
+          // handle postsubmit callback
+          onSuccess(response)
         }
 
       },
       features: {
         // Used to enable registration feature on the widget.
         // https://github.com/okta/okta-signin-widget#feature-flags
-         registration: true // REQUIRED
+        registration: true // REQUIRED
       }
-    
-    });
-    this.widget.renderEl({el}, this.props.onSuccess, this.props.onError);
+
+    })
+    this.widget.renderEl({ el }, this.props.onSuccess, this.props.onError,
+      )
   }
 
-  componentWillUnmount() {
-    this.widget.remove();
+  componentWillUnmount () {
+    this.widget.remove()
   }
 
-  render() {
-    return <div />;
+  render () {
+    return <div />
   }
 };
--- client/CLIENTREACTREDUX/src/components/StyledCard.js
+++ client/CLIENTREACTREDUX/src/components/StyledCard.js
@@ -1,16 +1,14 @@
 import styled from 'styled-components'
 
 const StyledCard = styled.div`
-  box-shadow: 5px 5px 5px #ccc;
-  border: 1px solid #ccc;
-  height: 2000px;
-  width: 750px;
-  overflow: scroll;
+  height: 25vh;
+  width: 25vw;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
   margin: 0 auto;
+  background-color: white;
 `
 
 export default StyledCard
