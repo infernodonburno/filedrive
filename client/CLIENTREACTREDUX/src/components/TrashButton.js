602a81fb549b0e513bab978fedf7541781b42ba5
Courtney <lrccoordntr@aol.com>
2019-10-24 11:10:35 -0500

    Fixed issue with trash and delete icons not showing

--- client/CLIENTREACTREDUX/src/components/TrashButton.js
+++ client/CLIENTREACTREDUX/src/components/TrashButton.js
@@ -5,7 +5,7 @@ const TrashButton = props => {
     <div>
       <input
         type='image'
-        src='http://localhost:8081/src/resources/trashicon.png'
+        src='./resources/trashicon.png'
         onClick={props.onClick}
         alt='trashbutton'
       />
